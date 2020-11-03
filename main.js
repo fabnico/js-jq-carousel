a0678e39396cb9b6955178737cd124630aaef4ce
Fabrizio <whoet.wo@gmail.com>
2020-11-03 16:48:42 +0100

    slider on keydown inserito

--- main.js
+++ main.js
@@ -21,3 +21,31 @@ $('.prev').click(function(){
       $('img.last').addClass('active')
    }
 })
+
+$(document).keydown(function(left){
+  if(left.keyCode == 37) {
+     var actImg = $('img.active')
+
+     actImg.removeClass('active')
+
+     if(!actImg.hasClass('first')){
+        actImg.prev().addClass('active')
+     }else{
+        $('img.last').addClass('active')
+     }
+  }
+})
+
+$(document).keydown(function(right){
+  if(right.keyCode == 39) {
+     var actImg = $('img.active')
+
+     actImg.removeClass('active')
+
+     if(!actImg.hasClass('last')){
+        actImg.next().addClass('active')
+     }else{
+        $('img.first').addClass('active')
+     }
+  }
+})
