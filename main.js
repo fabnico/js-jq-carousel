73eaa6f73796c60d8be0bafd4285590a4f8d8ff9
Fabrizio <whoet.wo@gmail.com>
2020-11-03 17:25:08 +0100

    slider dot blu on click inseriti

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 $('.next').click(function(){
    var actImg = $('img.active')
+   var actDot = $('i.active')
 
    actImg.removeClass('active')
+   actDot.removeClass('active')
 
    if(!actImg.hasClass('last')){
       actImg.next().addClass('active')
    }else{
       $('img.first').addClass('active')
    }
+
+   if(!actDot.hasClass('last')){
+      actDot.next().addClass('active')
+   }else{
+      $('i.first').addClass('active')
+   }
+
 })
 
 $('.prev').click(function(){
    var actImg = $('img.active')
+   var actDot = $('i.active')
 
    actImg.removeClass('active')
+   actDot.removeClass('active')
 
    if(!actImg.hasClass('first')){
       actImg.prev().addClass('active')
    }else{
       $('img.last').addClass('active')
+   }
+
+   if(!actDot.hasClass('first')){
+      actDot.prev().addClass('active')
+   }else{
+      $('i.last').addClass('active')
    }
 })
 
@@ -34,6 +51,7 @@ $(document).keydown(function(left){
         $('img.last').addClass('active')
      }
   }
+
 })
 
 $(document).keydown(function(right){
