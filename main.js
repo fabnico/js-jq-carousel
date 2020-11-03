ef0a9284c2548ec825efebbb406da2fd52c47fee
Fabrizio <whoet.wo@gmail.com>
2020-11-03 22:36:56 +0100

    ADD functions

--- main.js
+++ main.js
@@ -1,85 +1,63 @@
-$('.next').click(function(){
+function imgForward(){
    var actImg = $('img.active')
-   var actDot = $('i.active')
-
    actImg.removeClass('active')
-   actDot.removeClass('active')
-
    if(!actImg.hasClass('last')){
       actImg.next().addClass('active')
    }else{
       $('img.first').addClass('active')
    }
+}
 
+function dotForward(){
+   var actDot = $('i.active')
+   actDot.removeClass('active')
    if(!actDot.hasClass('last')){
       actDot.next().addClass('active')
    }else{
       $('i.first').addClass('active')
    }
+}
 
-})
-
-$('.prev').click(function(){
+function imgBack(){
    var actImg = $('img.active')
-   var actDot = $('i.active')
-
    actImg.removeClass('active')
-   actDot.removeClass('active')
-
    if(!actImg.hasClass('first')){
       actImg.prev().addClass('active')
    }else{
       $('img.last').addClass('active')
    }
+}
 
+function dotBack(){
+   var actDot = $('i.active')
+   actDot.removeClass('active')
    if(!actDot.hasClass('first')){
       actDot.prev().addClass('active')
    }else{
       $('i.last').addClass('active')
    }
+}
+
+$('.next').click(function(){
+   imgForward()
+   dotForward()
 })
 
-$(document).keydown(function(left){
-  if(left.keyCode == 37) {
-     var actImg = $('img.active')
-    var actDot = $('i.active')
-
-    actImg.removeClass('active')
-    actDot.removeClass('active')
-
-    if(!actImg.hasClass('first')){
-       actImg.prev().addClass('active')
-    }else{
-       $('img.last').addClass('active')
-    }
-
-    if(!actDot.hasClass('first')){
-       actDot.prev().addClass('active')
-    }else{
-       $('i.last').addClass('active')
-    }
-  }
-
+$('.prev').click(function(){
+   imgBack()
+   dotBack()
 })
 
 $(document).keydown(function(right){
   if(right.keyCode == 39) {
-     var actImg = $('img.active')
-    var actDot = $('i.active')
-
-    actImg.removeClass('active')
-    actDot.removeClass('active')
-
-    if(!actImg.hasClass('last')){
-       actImg.next().addClass('active')
-    }else{
-       $('img.first').addClass('active')
-    }
-
-    if(!actDot.hasClass('last')){
-       actDot.next().addClass('active')
-    }else{
-       $('i.first').addClass('active')
-    }
+    imgForward()
+    dotForward()
   }
+})
+
+$(document).keydown(function(left){
+   if(left.keyCode == 37) {
+   imgBack()
+   dotBack()
+   }
 })
